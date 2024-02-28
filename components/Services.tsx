0e189d837224516773ec4c309377d4b8fcd0b598
Junaidahmed111 <ahmedjuniad30@gmail.com>
2024-02-28 23:56:06 +0500

    services section completed

--- components/Services.tsx
+++ components/Services.tsx
@@ -6,30 +6,54 @@ import serviceThree from "@/public/assets/service-three.png";
 
 const Services = () => {
   return (
-    <section className="bg-red-200 pt-8 px-10">
+    <section className="bg-red-200 pt-8 px-16 flex flex-col gap-y-4">
       <span className="flex text-center items-center justify-center text-3xl font-bold">
         {" "}
         Our Services
       </span>
-      <div className="flex flex-col items-center justify-center gap-y-8 pt-4">
-        <div className="relative text-center">
-          <Image
-            src={serviceOne}
-            alt=""
-            width={1000}
-            height={1000}
-            className="h-[200px] sm:h-[200px] md:h-[300px] lg:h-[400px] w-screen "
-          />
-          <span className="absolute text-center bottom-[45%] left-[40%] font-bold md:text-xl sm:text-lg text-sm lg:text-2xl xl:text-4xl text-white">
+
+      <div className="flex flex-col items-center justify-center pt-4 relative">
+        <Image
+          src={serviceOne}
+          alt=""
+          width={1000}
+          height={1000}
+          className="h-[200px] sm:h-[200px] md:h-[300px] lg:h-[400px] w-screen"
+        />
+        <span className="absolute inset-0 flex items-center justify-center font-bold text-center text-white">
+          <span className="text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-4xl">
             New Home Builds
           </span>
-        </div>
-        <div className="">
-          <Image src={serviceTwo} alt="" />
-        </div>
-        <div className="">
-          <Image src={serviceThree} alt="" />
-        </div>
+        </span>
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-y-8 pt-4 relative">
+        <Image
+          src={serviceTwo}
+          alt=""
+          width={1000}
+          height={1000}
+          className="h-[200px] sm:h-[200px] md:h-[300px] lg:h-[400px] w-screen"
+        />
+        <span className="absolute inset-0 flex items-center justify-center font-bold text-center text-white">
+          <span className="text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-4xl">
+            New Home Builds
+          </span>
+        </span>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-y-8 pt-4 relative">
+        <Image
+          src={serviceThree}
+          alt=""
+          width={1000}
+          height={1000}
+          className="h-[200px] sm:h-[200px] md:h-[300px] lg:h-[400px] w-screen"
+        />
+        <span className="absolute inset-0 flex items-center justify-center font-bold text-center text-white">
+          <span className="text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-4xl">
+            New Home Builds
+          </span>
+        </span>
       </div>
     </section>
   );
